72cacce7f47d22e0d0754e4479ed6072c8a3f4f6
Zoya Ejaz <zejaz6806@gmail.com>
2026-01-03 02:38:43 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,11 @@ const navItems = [
   { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
+  {name: "Education", href: "#education"},
+  { name: "Certificates", href: "#certificates"},
   { name: "Projects", href: "#projects" },
-  { name: "Contact", href: "#contact" },
+  { name: "Experience", href: "#experience"}, 
+  { name: "Contact", href: "#contact"},
 ];
 
 export const Navbar = () => {
